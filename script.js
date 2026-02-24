0c600b4d3d3c71927181eeb0599ee3cf8fb2ba98
haryramirez08-ops <haryramirez08@gmail.com>
2026-02-24 10:56:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+// SLIDER
+const slides = document.querySelector(".slides");
+const images = document.querySelectorAll(".slides img");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+let index = 0;
+
+function showSlide(i) {
+    if (i >= images.length) index = 0;
+    if (i < 0) index = images.length - 1;
+    slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+next.addEventListener("click", () => {
+    index++;
+    showSlide(index);
+});
+
+prev.addEventListener("click", () => {
+    index--;
+    showSlide(index);
+});
+
+// Auto Slide
+setInterval(() => {
+    index++;
+    showSlide(index);
+}, 4000);
